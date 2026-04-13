f01b5134f9cc6b117f1eea8c83d36f8f375b10ec
Júlio Muanza Monteiro <juliomuanzam@gmail.com>
2026-04-13 22:50:50 +0100

    Update version and refactor enums in Verne_Code7448
    Updated version number and modified enum structure for counting digits and types.

--- src/Verne_Code7448.h
+++ src/Verne_Code7448.h
@@ -1,7 +1,7 @@
 /*
 	Nome: Verne_Code7448
 	Copyright: Verne_Code7448
-	Versão 1.0.0
+	Versão 1.0.1
 	Autor: Júlio Muanza Monteiro
 	Data: 11/04/26 20:26
 	Descrição: Controle do decodificador 7448
@@ -12,36 +12,50 @@
 
 #include "Arduino.h"
 
-enum numero {
-	ZERO, 
-	UM, 
-	DOIS, 
-	TRES, 
-	QUATRO, 
-	CINCO, 
-	SEIS, 
-	SETE, 
-	OITO, 
-	NOVE
+
+enum digitos { // Digito para a contagem
+	ZERO, 		// dígito 0
+	UM, 		// dígito 1
+	DOIS, 		// dígito 2
+	TRES, 		// dígito 3
+	QUATRO, 	// dígito 4
+	CINCO, 		// dígito 5
+	SEIS, 		// dígito 6
+	SETE, 		// dígito 7
+	OITO, 		// dígito 8
+	NOVE 		// dígito 9
 };
 
-class Verne_Code7448 {
+enum typeCont { // tipo de contagem
+	PROGRESS, 		// Progressiva
+	REGRESS 		// Regressiva
+};
+
+
+// Classe Verne_Code7448
+class Verne_Code7448 
+{
 	private:
-		// Quantidade de pinos no decodificador
+		// Quantidade de pinos do decodificador
 		static const int pinos = 4;
 		// pinos do decodificador 7448
 		int pinCode[pinos];
-		
+				
 	public:
 		Verne_Code7448(int d, int c, int b, int a); // Construtor Verne_Code7448
 		
-		// ----- Métodos -----
-		void digito(int digito); // Mostrar digito
-		void contagemProgressiva(int digito); // Contagem progressiva de 0-9
-		void contagemRegressiva(int digito); // Contagem regressiva de 9-0
-		void tipoContagem(int digito, bool estadoContagem); // Contagem progressiva (0-9) ou regressiva (0-9)
-		void limpar();
+
+		// ---------- Métodos ----------
+
+		void digito(int digito); // Mostrar um dígito no display
+
+		void cont_progress(int digito); // Fazer contagem progressiva de 0-9
+
+		void cont_regress(int digito); // Fazer contagem regressiva de 9-0
+
+		void cont(int digito, int type_cont, int time); // Escolhe o tipo de contagem entre progressiva ou regressiva
+
+		void clear();
 };
 
-#endif
-
+#endif // VERNE_CODE_7448_H
