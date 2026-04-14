e0c4b76737c3b8b7eaa369eb8bd2f7bc901d7c10
Júlio Muanza Monteiro <juliomuanzam@gmail.com>
2026-04-15 00:23:36 +0100

    Revise loop for countdown and digit display
    Updated loop function to implement countdown and display digits.

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,28 +1,31 @@
 #include <Verne_Code7448.h>
 
+// Criando um instância display do tipo Verne_Code7448
 Verne_Code7448 display(2, 3, 4, 5);
-int digito = 5;
-bool estado = true;
 
 void setup() {
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  display.digito(SEIS);
+void loop() 
+{
+  // Realiza uma contagem regressiva de 9-0 como tempo de troca entre dígitos de 1s
+  display.cont(9, PROGRESS, 200);
+
+  // Imprime digitos direitos no display
+  display.digito(3);  // dígito 3
   delay(1000);
-  display.digito(9);
+  display.digito(UM);  // dígito 1
   delay(1000);
-  display.digito(DOIS);
+  display.digito(4);  // dígito 4
+  delay(1000);
+  display.digito(OITO);  // dígito 8
   delay(1000);
 
-  estado = false;
-  display.tipoContagem(digito, estado);
-  estado = true;
-  display.tipoContagem(digito, estado);
+  display.clear(); // Limpa o display
+  delay(1000);
 
-  display.limpar();
-  delay(3000);
-
-
+  /*
+  display.cont_regress(3); // Contagem regressiva
+  display.cont_progress(8); // Contagem progressiva
+  */
 }
